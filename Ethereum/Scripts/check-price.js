011af7e0913d087265364707c0c911d61eede44f
Tim Steffes <tim.steffes@gmail.com>
2017-09-17 16:58:11 -0400

    add colored price differ

--- Ethereum/Scripts/check-price.js
+++ Ethereum/Scripts/check-price.js
@@ -1,17 +1,21 @@
-const https = require('https');
-
-setInterval(() =>
-{		
-  https.get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD', (resp) => {
-    let data = ''; 
-    resp.on('data', (chunk) => {
-    data += chunk;
-    });
-    resp.on('end', () => {
-      let result = JSON.parse(data);
-      console.log('Eth price: $' + result.USD + ' / ' + result.BTC + ' BTC');
-    });
-  }).on('error', (err) => {
-    console.log('Error: ' + err.message);
-  });
-}, 2000);
+    const https = require('https');
+    const chalk = require('chalk');
+    let curPrice = 0;
+    setInterval(() =>
+    {		
+      https.get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD', (resp) => {
+        let data = ''; 
+        resp.on('data', (chunk) => {
+        data += chunk;
+        });
+        resp.on('end', () => {
+          let result = JSON.parse(data);
+          if (curPrice == 0) curPrice = result.USD;
+          let diff = (result.USD - curPrice).toFixed(2);
+          curPrice = result.USD;
+          console.log('Eth price: $' + result.USD.toFixed(2) + ' / ' + result.BTC + ' BTC ' + (diff > 0 ? chalk.green('+' + diff) : diff < 0 ? chalk.red(diff) : ''));
+        });
+      }).on('error', (err) => {
+        console.log('Error: ' + err.message);
+      });
+    }, 2000);
